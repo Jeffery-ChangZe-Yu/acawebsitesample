05b78a18e1544fd8f754e814863d189d0e2bdc9f
Jeffery-ChangZe-Yu <57264662+Jeffery-ChangZe-Yu@users.noreply.github.com>
2025-09-18 00:08:12 -0700

    Edited Image Sizes for mobile

--- aca-website-sample/src/components/Chefpages/Chefpages.tsx
+++ aca-website-sample/src/components/Chefpages/Chefpages.tsx
@@ -1,38 +1,44 @@
 import React from "react";
 import acaLogo from "../../media/aca-logo.png";
+import { Link } from "react-router";
 
 const tempChefDirectory = [
     {
         name: "Martin Yan",
         img: acaLogo,
         title: "Honorary Advisor of ACA",
-        bio: "World Renowned Celebrity Chef, Television Cooking Show Host, Cookbook Author"
+        bio: "World Renowned Celebrity Chef, Television Cooking Show Host, Cookbook Author",
+        link: "/chefs/martin-yan"
     },
     {
 
         name: "Jet Tila",
         img: acaLogo,
         title: "Honorary Advisor of ACA",
-        bio: "Renowned Thai-American chef and star of Iron Chef, Cookbook Author, Restauranteur"
+        bio: "Renowned Thai-American chef and star of Iron Chef, Cookbook Author, Restauranteur",
+        link: ""
     },
     {
         name: "Shirley Cheng",
         img: acaLogo,
         title: "Vice President of ACA Central Region",
-        bio: "Retired Professor of CIA/HAAC, Asian Culinary Education Expert"
+        bio: "Retired Professor of CIA/HAAC, Asian Culinary Education Expert",
+        link: ""
 
     },
     {
         name: "Anthony Jung",
         img: acaLogo,
         title: "Vice President of ACA Northeast Region",
-        bio: "Multiple national gold medalist, Executive Sous Chef at the University of Massachusetts"
+        bio: "Multiple national gold medalist, Executive Sous Chef at the University of Massachusetts",
+        link: ""
     },
     {
         name: "William Yee",
         img: acaLogo,
         title: "Vice President of ACA Western Region",
-        bio: "Acclaimed California TV culinary showhost, past President of Le Cordon Bleu Alumni Association"
+        bio: "Acclaimed California TV culinary showhost, past President of Le Cordon Bleu Alumni Association",
+        link: ""
     }
 ]
 
@@ -41,20 +47,22 @@ function Chefpages() {
     return (
     <div className="content">
         <h1>Active Chef Directory</h1>
-        {tempChefDirectory.map(({name, title, img, bio}, idx) => {
+        {tempChefDirectory.map(({name, title, img, bio, link}, idx) => {
             return (
-            <div key={idx} className="chef-line">
-                <div className="chef-line--pi">
-                    <img className="chef-line--img"
-                    src={img} alt="poggies"/>
-                    <h2>{name}</h2>
+            <Link to={link}>
+                <div key={idx} className="chef-line">
+                    <div className="chef-line--pi">
+                        <img className="chef-line--img"
+                        src={img} alt="poggies"/>
+                        <h2>{name}</h2>
+                    </div>
+                    <div className="chef-line--profile">
+                        <h3>{title}</h3>
+                        <p>{bio}</p>
+                    </div>
                 </div>
-                <div className="chef-line--profile">
-                    <h3>{title}</h3>
-                    <p>{bio}</p>
-                </div>
-            </div>)
-        })}
+            </Link>
+            )})}
     </div>
     )
 }
